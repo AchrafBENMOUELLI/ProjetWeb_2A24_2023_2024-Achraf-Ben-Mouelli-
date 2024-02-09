bb6c0d4f40443d38f643225d70b763fac1f1100a
AchrafBENMOUELLI <benmouelliachraf@gmail.com>
2024-02-09 23:00:50 +0100

    update

---
+++ java/javascript.js
@@ -0,0 +1,41 @@
+function test(){
+    alert('hello 2A24');return false;
+}
+/*
+console.log("ceci un test");
+let no=prompt("saisir votre nom","votre nom");
+console.log(typeof(no));
+let noo=confirm("voulez vous quittez la salle");
+console.log(typeof(noo));
+var a=5;
+console.log("la valeur de a est " + a);
+var b=6;
+console.log("la valeur de b est " + b);
+let x,y;
+console.log("valeur de x est " + x);
+console.log("valeur de y est " + y);
+
+if (true){
+    var z=2020;
+}
+console.log(z);
+
+if (true){
+    let t = 'hello';
+    console.log(t);
+}
+
+
+let m="test";
+console.log(typeof(m));
+const PI=3.14;
+*/
+var arr1 = [5];
+var arr2 = Array(5);
+var arr3 = Array(5,2);
+var arr4 = Array.of(5);
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
